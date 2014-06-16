1f46ee2ee428fe4b72c0b1093055b16f202d1867
Mike Jensen <mike@RECON-054-L7.(none)>
2014-06-16 16:45:16 -0700

    starting to add zoom slider and edit text/text view combo for tracing

--- TypefaceTrace/gen/com/example/typefacetrace/R.java
+++ TypefaceTrace/gen/com/example/typefacetrace/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05004a;
+        public static final int action_settings=0x7f05004b;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int bBrowse=0x7f050040;
@@ -1101,14 +1101,14 @@ containing a value of this type.
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int decreaseTextSize=0x7f050048;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
-        public static final int etTrace=0x7f050049;
+        public static final int etTraceSize=0x7f050048;
+        public static final int etTraceText=0x7f050047;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
         public static final int home=0x7f050014;
@@ -1116,7 +1116,6 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int increaseTextSize=0x7f050047;
         public static final int label=0x7f050043;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
@@ -1128,6 +1127,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int sbZoomBar=0x7f05004a;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1145,12 +1145,13 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textOptions=0x7f050046;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int traceLayout=0x7f050044;
+        public static final int tracingProperties=0x7f050045;
         public static final int tvBrowse=0x7f05003c;
-        public static final int tvTrace=0x7f050045;
+        public static final int tvTrace=0x7f050046;
+        public static final int tvTraceText=0x7f050049;
         public static final int tvWelcome=0x7f05003e;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
